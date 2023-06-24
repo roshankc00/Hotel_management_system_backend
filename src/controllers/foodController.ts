02e6cd6439a8e387387b3d030068fc47a2a4960e
Roshan karki <rohitkc8848@gmail.com>
2023-06-24 08:04:43 +0545

    feat:added the food handlers

--- src/controllers/foodController.ts
+++ src/controllers/foodController.ts
@@ -1,6 +1,10 @@
 import asyncHandler from 'express-async-handler';
 import { Request,Response } from 'express';
 import FoodModel from '../models/foodModel';
+import validateMongodbId from '../utils/mongodbIdValidator';
+import { CustomRequest } from '../interfaces/user.interfaces';
+
+// create the food 
 export const createFood=asyncHandler(async(req:Request,res)=>{
     try {
         const {name,price,discountPer,category}=req.body;
@@ -25,7 +29,7 @@ export const createFood=asyncHandler(async(req:Request,res)=>{
 })
 
 
-
+// get all the foods 
 export const getALLFoods=asyncHandler(async(req,res:any)=>{
     try {
         const foods=await FoodModel.find({})
@@ -39,5 +43,111 @@ export const getALLFoods=asyncHandler(async(req,res:any)=>{
     } catch (error:any) {
         
         throw new Error(error)
+    }
+})
+
+// get  a single food 
+export const getSingleFood=asyncHandler(async(req:Request,res:Response)=>{
+    try {
+        const id=req.params.id
+        validateMongodbId(id)
+        const food=await FoodModel.findById(id)
+        if(!food){
+            throw new Error("food doesnt exists")
+        }else{
+            res.status(200).json({
+                sucess:true,
+                food
+            })
+        }
+        
+    } catch (error:any) {
+        throw new Error(error)
+        
+    }
+
+})
+
+
+// update the food
+export const updateFood=asyncHandler(async(req:any,res:Response)=>{
+    try {
+        const id=req.params.id
+        validateMongodbId(id)
+        const food=await FoodModel.findById(id)
+        if(!food){
+            throw new Error("food doesnt exists")
+        }
+        const updFood=await FoodModel.findByIdAndUpdate(id,req.body,{new:true})
+
+        res.status(200).json({
+            sucess:true,
+            message:"food updated sucessfully",
+            updFood
+        })
+        
+    } catch (error:any) {
+        throw new Error(error)
+    }
+})
+
+
+
+
+// delete the food 
+export const deleteFood=asyncHandler(async(req:Request,res:Response)=>{
+    try {
+        const id=req.params.id
+        validateMongodbId(id)
+        const food=await FoodModel.findById(id)
+        if(!food){
+            throw new Error("food doesnt exists")
+        }
+        const delFood=await FoodModel.findByIdAndDelete(id)
+        res.status(200).json({
+            sucess:true,
+            messagea:"deleted sucess fully"
+        })
+        
+    } catch (error:any) {
+        throw new Error(error)
+        
+    }
+
+})
+
+
+// get a product by category
+export const getFoodsByCategory=asyncHandler(async(req,res)=>{
+    try {
+        let {category}=req.body
+        let foods=await FoodModel.find({category})
+        if(!foods){
+            throw new Error("no food found")
+        }
+        res.status(200).json({
+            sucess:true,
+            foods
+        })
+
+    } catch (error:any) {
+        throw new Error(error)        
+    }
+})
+
+// get foods  with name price discountper priceAfterDiscount
+export const foodsWithLimitedField=asyncHandler(async(req,res)=>{
+    try {
+        const product=await FoodModel.find({}).select('name price discountPer priceAfterDiscount')
+        let length=product.length
+        res.status(200).json({
+            sucess:true,
+            product,
+            length
+
+        })
+    } catch (error:any) {
+        throw new Error(error)
+        
     }
 })
--- src/routes/foodRoute.ts
+++ src/routes/foodRoute.ts
@@ -1,10 +1,16 @@
 import express from 'express'
-import { createFood, getALLFoods } from '../controllers/foodController'
+import { createFood, deleteFood, foodsWithLimitedField, getALLFoods, getFoodsByCategory, getSingleFood, updateFood } from '../controllers/foodController'
 import { filterResults } from '../middlewares/filter_sort_pagination'
 import FoodModel from '../models/foodModel'
+import { checkAuth, checkRole } from '../middlewares/authmiddleware'
 const router=express.Router()
 
 router.post('/food/create',createFood)
 router.get('/foods',filterResults(FoodModel),getALLFoods)
+router.delete('/food/:id',checkAuth,checkRole('admin'),deleteFood)
+router.get('/food/:id',getSingleFood)
+router.put('/food/:id',checkAuth,checkRole('admin'),updateFood)
+router.post('/foods/category',getFoodsByCategory)
+router.get('/foods/limit',foodsWithLimitedField)
 
 export default router
